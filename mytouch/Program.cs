1836b87eac889b3fbcaef7bf621d1c9476d82a84
Aaron Resch <aaronmresch@gmail.com>
2021-03-06 11:20:00 -0600

    added mytouch

---
+++ mytouch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("USAGE: mytouch <filename> (optional: <filename2> <filename3> ...)");
+    Environment.Exit(1);
+}
+
+foreach (string filename in args)
+{
+    if (!File.Exists(filename))
+    {
+        File.Create(filename);
+    }
+    else
+    {
+        Console.Error.WriteLine($"Error: file already exists - {filename}");
+        continue;
+    }
+}
