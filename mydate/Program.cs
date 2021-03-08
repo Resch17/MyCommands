1609def8a332695cbb69a95819e7d69f04e04430
Aaron Resch <aaronmresch@gmail.com>
2021-03-08 15:52:06 -0600

    finished mydate (enough)

--- mydate/Program.cs
+++ mydate/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace mydate
 {
@@ -6,7 +7,15 @@ namespace mydate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(DateTime.Now.ToString("F"));
+            // The date should be printed in this format: "Mon Mar   8 11:54:05 AM CST 2021"
+            DateTime current = DateTime.Now;
+            TimeZoneInfo localZone = TimeZoneInfo.Local;
+            string dayMonthDate = current.ToString("ddd MMM d");
+            string time = current.ToString("T");
+            string year = current.ToString("yyyy");
+            string timeSetting = localZone.IsDaylightSavingTime(current) 
+                ? localZone.DaylightName : localZone.StandardName;
+            Console.WriteLine(dayMonthDate + " " + time + " " + timeSetting + " " + year);
         }
     }
 }
