5bfede925583cc8599f99d04133d365834e2e431
Aaron Resch <aaronmresch@gmail.com>
2021-03-07 21:08:28 -0600

    added mydate

---
+++ mydate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mydate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString("F"));
+        }
+    }
+}
