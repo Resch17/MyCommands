7d21ddbf1fd3978f443cd1960de4de2a23aaed72
Aaron Resch <aaronmresch@gmail.com>
2021-03-11 11:08:27 -0600

    finished mycp

---
+++ mycp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace mycp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("USAGE: mycp <fileToCopy> <destinationFilename>");
+                Environment.Exit(0);
+            }
+            string originFile = args[0];
+            string destinationFile = args[1];
+
+            try
+            {
+                File.Copy(originFile, destinationFile);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Could not copy - {ex.Message}");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
