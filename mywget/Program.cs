47081563b3cf5806a736423bebe9e645d1f9447a
Aaron Resch <aaronmresch@gmail.com>
2021-03-11 11:45:16 -0600

    finished mywget

---
+++ mywget/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+namespace mywget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("USAGE: mywget <file URL> <destination filename>");
+                Environment.Exit(0);
+            }
+            else
+            {
+                using (var client = new WebClient())
+                {
+                    try
+                    {
+                        client.DownloadFile(args[0], args[1]);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine($"Could not download file at: {args[0]}");
+                        Console.Error.WriteLine(ex.Message);
+                        Environment.Exit(0);
+                    }
+                }
+            }
+        }
+    }
+}
