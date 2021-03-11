1d69b5a017193935630e5731ba47506b907da36d
Aaron Resch <aaronmresch@gmail.com>
2021-03-11 11:28:41 -0600

    finished mymv

---
+++ mymv/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace mymmv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("USAGE: mymv <FILE1> <DESTINATION FILE OR DIR>");
+                Environment.Exit(0);
+            }
+
+            string file = args[0];
+            string destination = args[1];
+
+            try
+            {
+                File.Move(file, destination);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Couldn't execute mv on file: {file}");
+                Console.Error.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
