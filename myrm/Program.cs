84b78f24f7db638612d099a921a78093f8b039c1
Aaron Resch <aaronmresch@gmail.com>
2021-03-11 10:30:15 -0600

    finished myrm

---
+++ myrm/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace myrm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("USAGE: myrm <FILE> ...");
+                Environment.Exit(0);
+            }
+            foreach (string file in args)
+            {
+                try
+                {
+                    if (File.Exists(file))
+                    {
+                        File.Delete(file);
+                    }
+                    else
+                    {
+                        throw new Exception("File does not exist");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine($"Could not delete file {file}");
+                    Console.Error.WriteLine(ex.Message);
+                    continue;
+                }
+            }
+        }
+    }
+}
