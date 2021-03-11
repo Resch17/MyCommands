5dee515e210e6482b0d71152251111e940d4d596
Aaron Resch <aaronmresch@gmail.com>
2021-03-11 10:14:12 -0600

    finished mymkdir

---
+++ mymkdir/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace mymkdir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("USAGE: mymkdir <directory>");
+                Environment.Exit(0);
+            }
+
+            foreach (string path in args)
+            {
+                if (Directory.Exists(path))
+                {
+                    Console.WriteLine($"Directory {path} already exists");
+                    continue;
+                }
+                else
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
+        }
+    }
+}
